d22c7090ffb58885e584e1e7d6bff96527474e63
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-05-08 12:21:08 +0530

    first commit

---
+++ creation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# arr = np.array([10,20,30,40])
+# print(arr[0:3])
+
+arr = np.array([[10,20,30,40],[20,40,60,80]])
+print(np.shape(arr))
+print(np.size(arr))
+print(np.ndim(arr))
+
+
+# [50,60,70,80]
+
+# print(arr)
+# print(type(arr))
